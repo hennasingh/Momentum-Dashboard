34e5ee9cf329b77b2579df69f73f72cff254c751
Henna Singh <hennasingh04@gmail.com>
2023-11-14 13:26:06 +0000

    implemented code to fetch crypto currency details

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const imageAuthor = document.getElementById('image-author')
 
-async function init() {
-    const response = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    const data = await response.json()
-    document.body.style.backgroundImage = `url("${data.urls.full}")`
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(response => response.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url("${data.urls.full}")`
+        imageAuthor.textContent = ` By: ${data.user.name}`
+    })
+    .catch(err => {
+        // Use a default background image/author
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`
+		document.getElementById("author").textContent = `By: Dodi Achmad`
+    })
 
-    imageAuthor.textContent = ` By: ${data.user.name}`
-}
+    fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        document.getElementById('crypto-icon').src = `${data.image.small}`
+        document.getElementById('crypto-name').textContent = data.name
+    })
+    .catch(err => {
+        console.error(err)
+    })
 
-init()
